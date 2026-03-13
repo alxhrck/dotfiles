dade46fed038e4f78ea36fefba9ac055806708a3
Alex Herrick <alxhrck@users.noreply.github.com>
2026-03-13 11:46:55 -0500

    updates across dot files

---
+++ zsh/env.zsh
@@ -0,0 +1,8 @@
+# Exports
+export HISTFILE="$HOME/.zsh_history"
+export HIST_STAMPS="yyyy-mm-dd"
+export LS_COLORS='no=00;37:fi=00;34:di=00;33:ln=04;36:pi=40;33:so=01;35:bd=40;33;01:'
+export PATH=$HOME/.local/bin:$PATH
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$XDG_CONFIG_HOME/local/share"
+export XDG_CACHE_HOME="$XDG_CONFIG_HOME/cache"
--- zsh/history.zsh
+++ zsh/history.zsh
@@ -48,7 +48,7 @@ setopt hist_ignore_dups       # ignore duplicated commands history list
 setopt hist_ignore_space      # ignore commands that start with space
 setopt hist_verify            # show command with history expansion to user before running it
 setopt inc_append_history     # save each command to the history file as soon as it is executed
-# setopt share_history          # share command history data
+setopt share_history          # share command history data
 
 
 
