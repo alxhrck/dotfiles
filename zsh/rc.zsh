c162656e9de53f3c94483fd6eb1f9383609fbdd0
Alex Herrick <alxhrck@users.noreply.github.com>
2026-03-18 23:13:43 -0500

    update keybindings

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -6,11 +6,10 @@ setopt interactivecomments # enable comments "#" expressions in the prompt shell
 setopt extended_glob
 setopt globdots
 setopt menucomplete
-setopt correct
+setopt CORRECT
 setopt AUTO_PUSHD           # Push the current directory visited on the stack.
 setopt PUSHD_IGNORE_DUPS    # Do not store duplicates in the stack.
 setopt PUSHD_SILENT         # Do not print the directory stack after pushd or popd.
-
 
 
 # Zsh Completion
@@ -27,35 +26,33 @@ zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
 zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
 
 # Fun Aliases
-alias ll="ls --color=auto -lhart --group-directories-first"  # list files with details
+alias la="ls --color=auto -Aouhtg --group-directories-first"  # list files with details
 alias ls="ls --color=auto --group-directories-first"
 alias rm="rm -i"  # show confirm prompt
-alias cat="batcat"
 alias grep="grep --color=auto" 
 alias ope='sudo $(fc -ln -1)'
 alias ..="cd .."
 alias ...="cd ../.."
 alias bathelp='batcat --plain --language=help'
+alias bat="batcat"
+alias cat="batcat"
+alias which="type -a" # 'type -a' is more robust in Zsh than 'which'
+alias psg="ps aux | grep"
 
 # Start tmux
-if [[ "$TMUX" = "" ]];
-then
-  tmux attach -t $(hostname) || tmux new -s $(hostname);
+if [[ -z "$TMUX" ]]; then
+    tmux attach -t $(hostname) || tmux new -s $(hostname)
+#else
 fi
 
 # Slammin Functions
 function help() { "$@" --help 2>&1 | bathelp }
 function random_password() { tr -dc "[:graph:]" < /dev/urandom | head -c${1:-31} && echo }
 function ssh() { tmux rename-window "$@"; /usr/bin/ssh $@; tmux set-window-option automatic-rename on }
-function source_if_exists () {
-    if test -r "$1"; then
-        source "$1"
-    fi
-}
+function source_if_exists () { if test -r "$1"; then source "$1"; fi }
 
 # Xtra Configurations
 source_if_exists ~/.config/history.zsh
-#source_if_exists ~/.config/zsh-autosuggestions.zsh
 source_if_exists ~/.zshrc.local # User configuration
 
 eval "$(starship init zsh)"
